8bba826368594f8dd073b74ada8614fffc7ee15c
Saulo Costa <saulomlcosta10@gmail.com>
2021-12-17 08:33:48 -0300

    Initiate the project and create the first structure.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySuperBank
+{
+  class Program 
+  {
+    static void Main(string[] args)
+    {     
+      var account = new BankAccount("Saulo", 10000);
+      Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");      
+    }
+
+  }
+}
---
+++ SecondProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SecondProjectMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
